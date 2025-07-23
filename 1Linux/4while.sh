a78f6e8dbf9b46123b48a07e0b2c085423f7d055
cyril <cryl91@gmail.com>
2025-07-24 02:45:40 +0530

    First

--- 1Linux/4while.sh
+++ 1Linux/4while.sh
@@ -13,8 +13,8 @@
 
 var=$1
 
-while IFS= read -r line; 
-do
+while IFS= read -r line; #IFS is a special shell variable that tells the shell what characters to use to split input into words. Default value: space, tab, and newline
+do #Without IFS the spaces in the lines are reduced to one space. While IFS Preserves the spaces.
     if [[ "$line" == *$var* ]]; then
         echo "Found it! --> $line"
         # Optionally exit the loop early
