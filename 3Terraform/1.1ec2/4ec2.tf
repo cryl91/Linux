b6b02a2518e111333319adbe845c9d21caeb5aae
cyril <cryl91@gmail.com>
2025-07-25 02:53:06 +0530

    First

--- 3Terraform/1.1ec2/4ec2.tf
+++ 3Terraform/1.1ec2/4ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "myinstance" {
   ami                     = "ami-08b5b3a93ed654d19"
   instance_type           = "t2.micro"
     tags = {
-      name = "mongodb"
+      Name = "mongodb"
       Envirnoment = "dev"
     }
      } 
