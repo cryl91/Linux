69de610ec9a51222623b534ea99ce8f4d1ba7d60
cyril <cryl91@gmail.com>
2025-07-25 21:25:39 +0530

    First

--- 3Terraform/1.1ec2/4ec2.tf
+++ 3Terraform/1.1ec2/4ec2.tf
@@ -1,6 +1,10 @@
 resource "aws_instance" "myinstance" {
   ami                     = "ami-08b5b3a93ed654d19"
   instance_type           = "t2.micro"
+
+   provisioner "local-exec" {
+    command = "echo ${self.private_ip} > file1"
+  } 
     tags = {
       Name = "mongodb" #Use capital "N" in name to give name to ec2 instance
       Envirnoment = "dev"
