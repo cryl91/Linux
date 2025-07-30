12ea46d71220d14265bd40282d160ba491aed5a5
cyril <cryl91@gmail.com>
2025-07-31 00:40:39 +0530

    First

--- 3Terraform/4tfvars/2variable.tf
+++ 3Terraform/4tfvars/2variable.tf
@@ -1,27 +1,18 @@
-variable "instance_name"{ 
-    default = "mongodb"
+variable "instances" {
+    default = "t2.micro"
 }
- 
 
 variable "ami_id"{
 type = string
 default = "ami-08b5b3a93ed654d19"
 }
 
-variable "instance_type"{
-type = string
-default = "t2.micro"
-}
-
 variable "tags" {
     type = map
     default = { 
-        name = "mongodb"
+        Name = "mongodb"
         Envirnoment = "dev"
     
     }
 }
 
-variable "instances" {
-    default = "t2.micro"
-}
