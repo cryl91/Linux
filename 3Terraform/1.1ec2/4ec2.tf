ded4c95a9594a153585cad5978f7809624e60be6
cyril <cryl91@gmail.com>
2025-08-05 01:53:42 +0530

    First

--- 3Terraform/1.1ec2/4ec2.tf
+++ 3Terraform/1.1ec2/4ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "myinstance" {
   ami                     = "ami-08b5b3a93ed654d19"
   instance_type           = "t2.micro"
+  user_data = file("install.sh") //Reads the shell script (install.sh) and passes it as user data and the script runs on the EC2 instance at launch. This is used inside an aws_instance resource. 
 
    provisioner "local-exec" {
     command = "echo ${self.private_ip} > file1"
