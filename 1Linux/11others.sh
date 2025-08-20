6f396e0c2675682efa08fa3eb529b1187ef91c01
cyril <cryl91@gmail.com>
2025-08-21 05:05:30 +0530

    First

--- 1Linux/11others.sh
+++ 1Linux/11others.sh
@@ -1,29 +1,6 @@
-#!/bin/bash
-#!/bin/bash
-
-# Directory to back up
-SRC_DIR="/var/log"
-
-# Backup destination
-DEST_DIR="/backup"
-
-# Create backup directory if it doesn't exist
-mkdir "$DEST_DIR"
-
-# Timestamp
-TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-
-# Backup filename
-BACKUP_FILE="$DEST_DIR/$TIMESTAMP.zip"
-
-# Run the backup = zip -r <O/P zip filename> <destination to be compressed>
-zip -r "$BACKUP_FILE" "$SRC_DIR" # You can also use tar command.
-
-# Print result
-echo "Backup created: $BACKUP_FILE"
-
-#So, if SRC_DIR="/var/log" and timestamp is 20250818_133045, it will create something like: /backup/20250818_133045.zip containing everything under /var/log. ✅
-
+#Compression is the process of reducing the size of files or directories so they take up less disk space and transfer faster over a network.
+# ZIP method = zip -r <O/P zip filename> <destination to be compressed>
+# Tar method = tar -cfz <O/P tar filename> <destination to be compressed ie .tar.gz>
 # zip -r "$BACKUP_FILE" "$SRC_DIR" means:
 #     1. zip = command to create a zip archive.
 #     2. -r = recursive (include all subdirectories).
@@ -33,8 +10,9 @@ echo "Backup created: $BACKUP_FILE"
 # On Linux .tar.gz is preferred over zip because it:
 #     1. Compresses better and faster than zip
 #     2. Preserves Linux file permissions, ownership, symlinks, etc.
+#     3. zip/unzip → good for Windows compatibility and tar/tar.gz → better for Linux backups (permissions, symlinks, performance).
 
-# Tar code is = tar -cfz <O/P zip filename> <destination to be compressed ie .tar.gz>
+# Tar code is = 
 # BACKUP_FILE="$DEST_DIR/log_backup_$TIMESTAMP.tar.gz" 
 # tar -czf "$BACKUP_FILE" "$SRC_DIR" 
 
@@ -48,4 +26,17 @@ echo "Backup created: $BACKUP_FILE"
 
 # To extract = 
 #     1. unzip archive.zip -d /path/to/extract = -d specifies where to extract (if omitted, it extracts into the current directory).
-#     2. tar -xzvf archive.tar.gz /path/to/extract = x is to extract
+#     2. tar -xzf archive.tar.gz /path/to/extract = x is to extract
+
+#!/bin/bash
+SRC_DIR="/var/log"
+DEST_DIR="/backup"
+mkdir "$DEST_DIR"
+# Timestamp
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S") # O/P is 20250820_232909
+BACKUP_FILE="$DEST_DIR/$TIMESTAMP.zip"
+zip -r "$BACKUP_FILE" "$SRC_DIR" # You can also use tar command.
+echo "Backup created: $BACKUP_FILE"
+
+#So, if SRC_DIR="/var/log" and timestamp is 20250818_133045, it will create something like: /backup/20250818_133045.zip containing everything under /var/log. 
+
