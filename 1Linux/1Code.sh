6e6a4c89ff0a2e89891486518518325ce06611fe
cyril <cryl91@gmail.com>
2025-09-09 01:52:45 +0530

    First

--- 1Linux/1Code.sh
+++ 1Linux/1Code.sh
@@ -4,3 +4,6 @@ echo "Yes"
 
 #To count the num of "devops" wprdom 15 html files = grep "devops" *.html | wc -l 
 #To run a simple python web server = python3 -m http.server 8000
+#To ssh = ssh -i /c/users/cyril/downloads/vpcpem.pem ec2-user@13.218.108.176   
+#To copy from local to ec2 server = scp -i C:/Users/cyril/Downloads/vpcpem.pem C:/Users/cyril/Downloads/vpcpem.pem ec2-user@13.218.108.176:/home/ec2-user/
+
