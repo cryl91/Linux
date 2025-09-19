37f6bec2e5f26b2aba30325836ed05a34479b15d
cyril <cryl91@gmail.com>
2025-09-19 23:08:59 +0530

    First

--- 3Terraform/8.1module/ec2_main_module/3main.tf
+++ 3Terraform/8.1module/ec2_main_module/3main.tf
@@ -1,6 +1,7 @@
 #In this folder you will apply terraform init,plan,apply
 module "ec2" { 
 source = "../ec2_modules"
+#To use terraform module registry = source = "terraform-aws-modules/vpc/aws"
 #To call github stored modules = "git::<https of github repo>"
 ami_id = var.ami_id
 instance_type = var.instance_type 
