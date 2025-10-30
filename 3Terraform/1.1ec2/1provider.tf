8a561beeee6618e2374f2fc356a8dd3750b8512e
cyril <cryl91@gmail.com>
2025-10-30 23:47:19 +0530

    First

--- 3Terraform/1.1ec2/1provider.tf
+++ 3Terraform/1.1ec2/1provider.tf
@@ -12,3 +12,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
+#Remote storage of state file in AWS S3
+# terraform {
+#   backend "s3" {
+#     bucket         = "my-terraform-state"  #S3 bucket name
+#     key            = "prod/terraform.tfstate"  #path to the state file 
+#     region         = "us-west-2"
+#     encrypt        = true
+#     dynamodb_table = "terraform-lock"  #optional for state locking
+#   }
+# }
