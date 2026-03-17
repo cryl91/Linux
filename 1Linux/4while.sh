0eee5f2f5d3d2870d46a3f32e75e57c923360b0d
cyril <cryl91@gmail.com>
2026-03-18 00:38:53 +0530

    First

--- 1Linux/4while.sh
+++ 1Linux/4while.sh
@@ -13,11 +13,11 @@
 
 var=$1
 
-while IFS= read -r user; #IFS is a special shell variable that tells the shell what characters to use to split input into words. Default value: space, tab, and newline
+while IFS= read user; #IFS (Internal Field Separator) is a special shell variable in Unix/Linux shells (like bash) that defines which characters are used to split input into words. IFS is a special shell variable that tells the shell what characters to use to split input into words. Default value: space, tab, and newline
 do #Without IFS the spaces in the lines are reduced to one space. While IFS Preserves the spaces.
     if [[ "$user" == *$var* ]]; then
         echo "Found it! --> $user"
         # Optionally exit the loop early
         break
-    fi
+    fi 
 done < 1Code.sh
